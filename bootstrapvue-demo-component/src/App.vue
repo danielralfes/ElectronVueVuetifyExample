b66275a232abad72593b050ae02f2791bf098b8a
Daniel Ralfes Silva de Abreu <danielralfes@gmail.com>
2020-11-10 11:11:04 -0300

    AppBootstrapvue

---
+++ bootstrapvue-demo-component/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Navbar/>
+    <HelloWorld/>
+  </div>
+</template>
+<script>
+import Navbar from './components/Navbar'
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'navbar',
+  components: {
+    Navbar,
+    HelloWorld
+  }
+}
+</script>
